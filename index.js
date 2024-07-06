9127431b9ec14c6a78ff2bba1d47b82fd0b16193
Vishwajeet Singh <vishwajeetsingh8422@gmail.com>
2024-07-06 13:34:30 +0530

    java script folder

---
+++ index.js
@@ -0,0 +1,14 @@
+// var n=10;
+// document.write(n*n*n);
+
+// var a = parseInt(prompt("Enter 1 no"));
+// var b = parseInt(prompt("Enter 2 number"));
+
+// alert(a+b);
+
+// var a =  parseInt(prompt("Enter a number to Cube"));
+// alert(a*a*a);
+
+var b = parseInt(prompt("enter breth"));
+var h = parseInt(prompt("enter hight"));
+alert(1/2*b*h);
