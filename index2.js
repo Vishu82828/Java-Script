79f7f122fb7240e984bb4e2829029819af7d2f7f
Vishwajeet Singh <vishwajeetsingh8422@gmail.com>
2024-07-06 19:32:27 +0530

    remark

--- index2.js
+++ index2.js
@@ -1,36 +1,79 @@
 
 //Switch case--------
 
-var n = parseInt(prompt("enter any number"));
-switch(n){
-    case 1:
-        alert("vishu");
-        break;
-    case 2:
-        alert("ankita");
-        break;
-    case 3:
-        alert("khusi");
-        break;
-    case 4:
-        alert("sourav");
-        break;
-    case 5:
-        alert("om");
-        break;
-    case 6:
-        alert("uttam");
-        break;
+// var n = parseInt(prompt("enter any number"));
+// switch(n){
+//     case 1:
+//         alert("vishu");
+//         break;
+//     case 2:
+//         alert("ankita");
+//         break;
+//     case 3:
+//         alert("khusi");
+//         break;
+//     case 4:
+//         alert("sourav");
+//         break;
+//     case 5:
+//         alert("om");
+//         break;
+//     case 6:
+//         alert("uttam");
+//         break;
     
-    default:
-        alert("i dont know anyone else")
-}
+//     default:
+//         alert("i dont know anyone else")
+// }
 
 // If else statement------------------
 
-var i = parseInt(prompt("enter your age"));
-if (i >= 18) {
-    alert("you are eligible");
-} else {
-    alert("you are not eligible");
-}
+// var i = parseInt(prompt("enter your age"));
+// if (i >= 18) {
+//     alert("you are eligible");
+// } else {
+//     alert("you are not eligible");
+// }
+
+
+//Switch case--------
+
+// var a = parseInt(prompt("enter first number"))
+// var b = parseInt(prompt("enter second number"))
+// var n = parseInt(prompt("enter any number 1=+, 2=-, 3=*, 4=%, 5=/"));
+// switch(n){
+//     case 1:
+//         alert(a+b);
+//         break;
+//     case 2:
+//         alert(a-b);
+//         break;
+//     case 3:
+//         alert(a*b);
+//         break;
+//     case 4:
+//         alert(a%b);
+//         break;
+//     case 5:
+//         alert(a/b);
+//         break;
+    
+//     default:
+//         alert("your input is invalid")
+// }
+
+
+// do while -----------
+// var n = parseInt(prompt("enter no to print from 1 to 10"));
+// var n = 1;
+// do{
+//     document.write(n)
+//     n++
+// }while(n<=10);
+
+ var n = parseInt(prompt("enter no to print table"));
+ var i = 10;
+ do{
+    document.write(n*i)
+    i--;
+ } while (i>=1)
