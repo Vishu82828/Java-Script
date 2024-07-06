3ba0f314499a5ba80273eb34a3c5e83911ccb03f
Vishwajeet Singh <vishwajeetsingh8422@gmail.com>
2024-07-06 16:40:39 +0530

    remark

---
+++ index2.js
@@ -0,0 +1,36 @@
+
+//Switch case--------
+
+var n = parseInt(prompt("enter any number"));
+switch(n){
+    case 1:
+        alert("vishu");
+        break;
+    case 2:
+        alert("ankita");
+        break;
+    case 3:
+        alert("khusi");
+        break;
+    case 4:
+        alert("sourav");
+        break;
+    case 5:
+        alert("om");
+        break;
+    case 6:
+        alert("uttam");
+        break;
+    
+    default:
+        alert("i dont know anyone else")
+}
+
+// If else statement------------------
+
+var i = parseInt(prompt("enter your age"));
+if (i >= 18) {
+    alert("you are eligible");
+} else {
+    alert("you are not eligible");
+}
