2e9f497c4cee6c2779a83cf105068828e3ff25bf
Vishwajeet Singh <vishwajeetsingh8422@gmail.com>
2024-07-10 00:23:22 +0530

    new files

---
+++ for.js
@@ -0,0 +1,26 @@
+// var n =5;
+// for(var i=1; i<=n; i++){
+//     for(var k=1; k<=n; k++);{
+//         document.write("*");
+//     }
+// }
+
+// for(var i=1; i<=5; i++){
+//     for(var k=1; k<=i; k++){
+//         document.write("* ");
+//     }
+//     document.write("<br>");
+// }
+
+// let a='5'
+// let b=5
+// document.write(a===b)
+
+// let k=30;
+// (k>15)? 
+// document.write("greater"):
+// document.write("smaller")
+
+// write a program to find a number is even or odd using ternary operator. -------------------------
+let n = parseInt(prompt("Enter a Number to find even or odd"))
+(n%2==0)?alert("even"):alert("odd")
