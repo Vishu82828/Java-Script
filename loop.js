991a4f0699ae3049439437b8004336089a8a5d2b
Vishwajeet Singh <vishwajeetsingh8422@gmail.com>
2024-07-12 19:17:16 +0530

    remark

---
+++ loop.js
@@ -0,0 +1,23 @@
+// let ar = []
+// sum = 0;
+// for(i=0;i<10;i++){
+//     ar [i] = parseInt(prompt("enter num to sum"))
+//     sum = sum + i;
+// }
+// document.write(sum);
+
+// Q 2.
+// Q2. write a program to print an odd no in array from 1 to 20
+// let n = []
+// for( let i=0; i<8; i++){
+//     n [i] = parseInt(prompt("Enter no to find odd bt 1 to 8"))
+//     if(n[i]%2!=0){
+//         document.write(n[i]);
+//     }
+// }
+
+let ar = [1,2,3,4,5,6,7,8,9,10]
+// for of
+for(let n in ar){
+    console.log(n);
+}
--- practice.js
+++ practice.js
@@ -12,16 +12,24 @@
 // Question 02
 // take 3 input from the user and check 1 input is even or odd . check 1 input is even so multiply 1 & 2 input. Check 1 input is odd so add 3 of them.
 // to check number is neutral or not
-let num1 = parseFloat(prompt("Enter the first number:"));
-let num2 = parseFloat(prompt("Enter the second number:"));
-let num3 = parseFloat(prompt("Enter the third number:"));
-function processInputs(num1, num2, num3) {
-    if (num1 % 2 === 0) {
-        let product = num1 * num2;
-        alert(`The product of ${num1} and ${num2} is ${product}.`);
-    } else {
-        let sum = num1 + num2 + num3;
-        alert(`The sum of ${num1}, ${num2}, and ${num3} is ${sum}.`);
-    }
-}
+// let num1 = parseFloat(prompt("Enter the first number:"));
+// let num2 = parseFloat(prompt("Enter the second number:"));
+// let num3 = parseFloat(prompt("Enter the third number:"));
+// function processInputs(num1, num2, num3) {
+//     if (num1 % 2 === 0) {
+//         let product = num1 * num2;
+//         alert(`The product of ${num1} and ${num2} is ${product}.`);
+//     } else {
+//         let sum = num1 + num2 + num3;
+//         alert(`The sum of ${num1}, ${num2}, and ${num3} is ${sum}.`);
+//     }
+// }
 
+var ar = []
+for (let i=0; i<10; i++){
+    ar [i] = parseInt(prompt("enter number"))
+    document.write(ar[i]);
+}
+// document.write(i)
+// Q1. write a program to add all 10 element in an Array
+// Q2. write a program to print an odd no in array from 1 to 20
