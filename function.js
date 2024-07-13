04afd21940399677ae6d1130912d2f0989250d55
Vishwajeet Singh <vishwajeetsingh8422@gmail.com>
2024-07-13 19:33:59 +0530

    remark

---
+++ function.js
@@ -0,0 +1,6 @@
+let n = [1,2,3,4,5,6,7]
+n.push(8); //add value in last
+n.pop();  //remove last element
+n.unshift("vishu"); //add element in first index of array
+n.shift(); // remove starting element of array
+document.write(n)
---
+++ tast.js
@@ -0,0 +1,13 @@
+// let a = parseInt(prompt("enter last first no"));
+// let b = parseInt(prompt("enter last second no"));
+// let c = parseInt(prompt("enter start first no"));
+// let d = parseInt(prompt("enter start second no"));
+// let ar = [10,8,7,5]
+// ar.push(a,b);
+// ar.unshift(c,d)
+// document.write(ar)
+
+let ar = [10,8,7,5,4,3,2,1]
+let pt = ar.slice(1,3) //slice is use for trim from array
+document.write(pt)
+// document.write(ar)
