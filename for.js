19db7497427407616059c587c00255623dcda313
Vishwajeet Singh <vishwajeetsingh8422@gmail.com>
2024-07-22 21:40:08 +0530

    remark

--- for.js
+++ for.js
@@ -5,12 +5,12 @@
 //     }
 // }
 
-// for(var i=1; i<=5; i++){
-//     for(var k=1; k<=i; k++){
-//         document.write("* ");
-//     }
-//     document.write("<br>");
-// }
+for(var i=1; i<=5; i++){
+    for(var k=1; k<=i; k++){
+        document.write("* ");
+    }
+    document.write("<br>");
+}
 
 // let a='5'
 // let b=5
@@ -22,5 +22,5 @@
 // document.write("smaller")
 
 // write a program to find a number is even or odd using ternary operator. -------------------------
-let n = parseInt(prompt("Enter a Number to find even or odd"));
-(n%2==0)?alert("even"):alert("odd")
+// let n = parseInt(prompt("Enter a Number to find even or odd"));
+// (n%2==0)?alert("even"):alert("odd")
