212eeccaba6cc1e34a944085cdc6321e9508240e
Vishwajeet Singh <vishwajeetsingh8422@gmail.com>
2024-08-01 12:56:25 +0530

    on mouse enter

---
+++ Change_img.js
@@ -0,0 +1,9 @@
+function change_img1(){
+    let img = document.getElementById('big_img')
+    img.src="img1.jpg"
+}
+
+function change_img2(){
+    let img = document.getElementById('big_img')
+    img.src="img2.jpg"
+}
---
+++ hover.js
@@ -0,0 +1,4 @@
+function pragraph(){
+    let change = document.getElementById('p_graph')
+    change.style.display="block"
+    }
