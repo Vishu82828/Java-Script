32ece76aac03b1bfbbc1c6772fe6c1c0dba57934
Vishwajeet Singh <vishwajeetsingh8422@gmail.com>
2024-08-13 18:48:21 +0530

    concat method

---
+++ Array_Method.js
@@ -0,0 +1,21 @@
+const array1 = ["BMW", "Audi", "Mercedes", "Suzaki" ]
+
+ console.log(array1)
+
+ array1.sort()
+ console.log(array1)
+
+ array1.reverse()
+ console.log(array1)
+
+ //document.getElementById("demo").innerHTML = array1
+
+ const number = [2,0,4,7,5,8,9,1,3]
+ console.log(number)
+
+ number.sort(function(a,b){return a-b})
+ console.log(number)
+
+ number.sort(function(a,b){return b-a})
+ console.log(number)
+
---
+++ string.js
@@ -0,0 +1,8 @@
+let x = "john"
+let y = "10"
+let z = "My name is \'john deo\' and he is john deo\'s friend\\";
+
+console.log(`hello, my name is ${x}, My age is ${y},`);
+
+
+//  It’s great to meet you. As a fresher with a background in software engineering.
