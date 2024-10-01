c815f4372d0eb9e2b42ace2ea9bfaf2093e7ea9d
Vishwajeet Singh <vishwajeetsingh8422@gmail.com>
2024-10-01 18:00:37 +0530

    json fetch

---
+++ fetch.js
@@ -0,0 +1,16 @@
+//  Featch - it proferm 5 types of function i-get, ii-deleate, iii-put, iv-patch, v-post.
+//  async await
+// promice -> object -> accept -> reject
+//console.log("hello") 
+
+let x = document.getElementById("btn").addEventListener("click",fch)
+async function fch(){
+    let storerData = await fetch('https://jsonplaceholder.typicode.com/comments')
+    let stringData = await storerData.json()
+    //console.log(stringData);
+    // let objData = stringData.reduce((acc, cur) => {
+    //     acc[cur.id] = cur;
+    //     return acc;
+    // }, {});
+    // console.log(objData);
+}
